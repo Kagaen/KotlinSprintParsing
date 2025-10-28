b3eca0eb551cf9002bb85e50622062109ef747d8
stasenko <alipsid@mail.ru>
2025-10-28 18:12:56 +0300

    Wiki parsing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,29 @@
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+
 fun main() {
 
+    val doc: Document = Jsoup.connect("https://en.wikipedia.org/").get()
+    println(doc.title())
+
+    // #Welcome_ti_Wikipedia
+    // .MainPageBG
+    // a
+    val newsHeadLines: Elements = doc.select("#mp-itn b a")
+//    println(newsHeadLines)
+
+    for (headLine: Element in newsHeadLines) {
+        println("${headLine.attr("title")}\n\t${headLine.absUrl("href")}")
+    }
+
 }
+
+//Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
+//log(doc.title());
+//Elements newsHeadlines = doc.select("#mp-itn b a");
+//for (Element headline : newsHeadlines) {
+//    log("%s\n\t%s",
+//        headline.attr("title"), headline.absUrl("href"));
+//}
